b9b155cd09114bac5271cf1474fabd97844c5772
groundhog3 <groundhog3@github.com>
2022-03-23 10:37:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/triviaapp/Controller.java
@@ -0,0 +1,16 @@
+package com.example.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Application;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+
+
+public class Controller extends Application {
+
+}
---
+++ app/src/main/java/com/example/triviaapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        readQuestionData();
+    }
+
+    public void readQuestionData(){
+        InputStream is = getResources().openRawResource(R.raw.trivia);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line = "";
+        try {
+            while((line = reader.readLine()) != null){
+                String[] fields = line.split(",");
+
+                Log.v("MainActivity", fields[0] + " " + fields[1]);
+
+                Question q = new Question(fields[0], fields[1]);
+            }
+        } catch(IOException e) {
+
+        }
+
+    }
+
+}
